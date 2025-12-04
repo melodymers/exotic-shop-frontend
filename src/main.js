594a413a1a041d76a1b588c6add02c2cde4d6e4c
melody <melodymeria2022@gmail.com>
2025-12-04 12:23:20 +0800

    style: added SCSS design system var

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
+import './assets/styles/variables.scss'
+
+// Bootstrap CSS + JS
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// Custom styles
+import './assets/styles/variables.scss'
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
